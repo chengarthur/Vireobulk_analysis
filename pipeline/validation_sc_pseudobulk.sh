3250b896dccd8951e31a4b829c3a77b86d792ed7
feifei <chengchen307@126.com>
2022-01-21 16:50:35 +0800

    Update validation_sc_pseudobulk.sh

--- pipeline/validation_sc_pseudobulk.sh
+++ pipeline/validation_sc_pseudobulk.sh
@@ -1,1 +1,14 @@
-/home/ccheng/subset-bam_linux" --bam "/media/zhanglab/scdata/pbmc89/outs/possorted_genome_bam.bam" --cell-barcodes barcode.tsv  --log-level info --cores 20 --out-bam pbmc_only_good_cell.bam
+#!/bin/sh
+#require：
+#https://github.com/10XGenomics/subset-bam
+#https://github.com/single-cell-genetics/cellsnp-lite
+#whitelist_raw_sc_bam
+subset-bam_linux --bam "cellranger_genome_bam.bam" --cell-barcodes whitelistbarcode.tsv  --log-level info --cores 20 --out-bam only_good_cell.bam
+
+#random sampling & index
+samtools index only_good_cell.bam
+samtools view only_good_cell.bam -s 0.8 -b -o 0.8coverage_good_cell.bam 
+
+#pileup
+cellsnp-lite -s "only_good_cell.bam" -O pbmc10donor -R ref_donors.vcf.gz -b whitelistbarcode.tsv --gzip -p 15 &
+
