b760d03e951128a31765992a839ee7d20e23ad74
feifei <chengchen307@126.com>
2023-11-21 10:46:25 +0800

    try.addnotebook

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ release = '0.0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 
-extensions = ['recommonmark']
+extensions = ['recommonmark','nbsphinx']
 
 
 # Add any paths that contain templates here, relative to this directory.
